e3f6d29a14f01b884cb9a8869565324c230b5997
Your Name <you@example.com>
2025-12-09 00:25:52 +0300

    tester

--- test_modules.py
+++ test_modules.py
@@ -1,16 +1,16 @@
 from modules import Greet, ADD, SUB, MUL, DIV
 
-def TEST_Greet():
+def test_Greet():
     assert Greet("TIM") == "Hello, dear TIM!"
 
-def TEST_ADD():
+def test_ADD():
     assert ADD(17, 13) == 30
 
-def TEST_SUB():
+def test_SUB():
     assert SUB(103, 63) == 40
 
-def TEST_MUL():
+def test_MUL():
     assert MUL(13, 5) == 65
 
-def TEST_DIV():
-    assert DIV(34, 7) == 2
+def test_DIV():
+    assert DIV(34, 17) == 2
