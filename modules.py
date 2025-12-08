2150602a35b6ebe50cc464ab3754cc16813efb4e
Your Name <you@example.com>
2025-12-09 00:12:39 +0300

    .github  .gitignore  modules.py tester  requirements

---
+++ modules.py
@@ -0,0 +1,14 @@
+def Greet(name: str):
+    return f"Hello, dear {name}!"
+
+def ADD(a: int, b: int):
+    return a + b
+
+def SUB(a: int, b: int):
+    return a - b
+
+def MUL(a: int, b: int):
+    return a * b
+
+def DIV(a: int, b: int):
+    return a / b
---
+++ test modules/test_modules.py
@@ -0,0 +1,16 @@
+from modules.py import Greet, ADD, SUB, MUL, DIV
+
+def TEST_Greet():
+    assert Greet("TIM") == "Hello, dear TIM!"
+
+def TEST_ADD():
+    assert ADD(17, 13) == 30
+
+def TEST_SUB():
+    assert SUB(103, 63) == 40
+
+def TEST_MUL():
+    assert MUL(13, 5) == 65
+
+def TEST_DIV():
+    assert DIV(34, 7) == 2
